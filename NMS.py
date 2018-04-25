4aeec38af8446651cdae16ca2876a8de475c3870
Arnaud <arnaudfournier921@gmail.com>
2018-04-25 11:42:15 +0200

    Add Non-Maxima Suppression fonction with tests

---
+++ NMS.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+# Fonction for doing non-maxima suppression of overlapping boxes.
+# Multiple boxes can be returned for the detection of a single face,
+# so this fonction will be used to eliminate the excess of boxes found.
+# It will keep only the box with best score and remove every other overlapping box.
+# Input :
+#   - boxes = a list of boxes corresponding to face detected by the classifier in one image
+#   - scores = a list of scores associated with each box
+# Output : the list of boxes to keep
+# Source : Adrian Rosebrock, (Faster) Non Maximum Suppression in Python.
+def non_max_suppression(boxes, scores, overlap_thres=0.5):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+
+	# if the boxes contain integers, convert them to floats for divisions.
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+
+	# initialize the list of picked indexes
+	pick = []
+
+	# retrieve the coordinates of the bounding boxes
+	# x1 = x coordinate of top-left corner
+	# y1 = y coordinate of top-left corner
+	# x2 = x coordinate of bottom-right corner
+	# y2 = y coordinate of bottom-right corner
+	x1 = boxes[:, 0]
+	y1 = boxes[:, 1]
+	x2 = boxes[:, 2]
+	y2 = boxes[:, 3]
+
+	# compute the area of every bounding box
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+	# sort the bounding boxes by score
+	idxs = np.argsort(scores)
+
+	# keep looping while some indexes still remain in the indexes list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the
+		# index value to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		# find the overlapping area of each box paired with the current box i
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+		# compute the width and height of overlapping area for each pair
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+
+		# compute the ratio of overlap for each pair
+		overlap = (w * h) / area[idxs[:last]]
+
+		# delete all indexes from the index list that have
+		# a sufficient overlapping with the current box
+		idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlap_thres)[0])))
+
+	# return only the bounding boxes that were picked using the
+	# integer data type
+	return boxes[pick].astype("int")
---
+++ test/NMSTest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from NMS import *
+import numpy as np
+
+
+# Test with only one detected face and two overlapping boxes.
+def test_with_1_detected_face():
+	box1 = np.array([13, 12, 17, 16])
+	box2 = np.array([13, 14, 17, 18])
+	box3 = np.array([14, 15, 18, 19])
+	boxes = np.vstack((box1, box2, box3))
+	scores = [0.78, 0.89, 0.77]
+	picked = non_max_suppression(boxes, scores)
+	assert (picked == boxes[1]).all()
+
+
+# test with 2 detected face, two boxes overlapping on the first one and the second box face overlapping
+# slightly on the first.
+def test_with_2_detected_face():
+	box1 = np.array([13, 12, 17, 16])
+	box2 = np.array([13, 14, 17, 18])
+	box3 = np.array([14, 15, 18, 19])
+	box4 = np.array([11, 16, 15, 20])
+	boxes = np.vstack((box1, box2, box3, box4))
+	scores = [0.65, 0.89, 0.77, 0.76]
+	picked = non_max_suppression(boxes, scores)
+	assert (picked == boxes[[1, 3]]).all()
